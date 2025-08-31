f5808228693e2343555df73844eaa2afc1377a4b
pavkas2024 <site.kasyanov@gmail.com>
2025-08-31 17:17:19 +0300

    update

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -1,15 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CloudinaryModule } from 'src/cloudinary/cloudinary.module';
-
+import { GoogleDriveModule } from '../google-drive/google-drive.module';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
-import { InstitutedocumentsSchema } from './schemas/documents.schema';
+import { InstitutedocumentSchema } from './schemas/documents.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Institutedocument', schema: InstitutedocumentsSchema }]),
-    CloudinaryModule,
+    MongooseModule.forFeature([{ name: 'Institutedocument', schema: InstitutedocumentSchema }]),
+     GoogleDriveModule,
   ],
   controllers: [DocumentsController],
   providers: [DocumentsService]
