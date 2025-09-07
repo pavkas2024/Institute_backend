77ef01d9eeb6de1ce03aca9a3a85c630c74c6563
pavkas2024 <site.kasyanov@gmail.com>
2025-09-07 16:05:46 +0300

    update

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DocumentsController } from './documents.controller';
 import { DocumentsService } from './documents.service';
+import { CloudinaryModule } from '../cloudinary/cloudinary.module'; 
 import { InstitutedocumentSchema } from './schemas/documents.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Institutedocument', schema: InstitutedocumentSchema }]),
+    CloudinaryModule, // <--- додати сюди
   ],
   controllers: [DocumentsController],
-  providers: [DocumentsService]
+  providers: [DocumentsService],
 })
 export class DocumentsModule {}
