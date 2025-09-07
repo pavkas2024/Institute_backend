92ff6321ff96fdaacc609ad27ce70547ecf61eb0
pavkas2024 <site.kasyanov@gmail.com>
2025-09-07 16:21:51 +0300

    update

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -35,31 +35,25 @@ export class CloudinaryService {
   async uploadPdf(
     file: Express.Multer.File,
   ): Promise<UploadApiErrorResponse | UploadApiResponse> {
-    return new Promise((resolve, reject) => {
+    return new Promise<UploadApiErrorResponse | UploadApiResponse>((resolve, reject) => {
       const upload = v2.uploader.upload_stream(
         {
-          resource_type: 'raw',
-          public_id: file.originalname.split('.')[0], // ім'я без розширення
-          use_filename: true,
-          unique_filename: false,
+          resource_type: 'raw',       
+          public_id: file.originalname.split('.')[0], 
+          use_filename: true,        
+          unique_filename: false,    
         },
         (error, result) => {
           if (error) return reject(error);
-
-      
-          if (result?.public_id) {
-            const cloudName = v2.config().cloud_name;
-            const extension = file.originalname.split('.').pop();
-            result.secure_url = `https://res.cloudinary.com/${cloudName}/raw/upload/fl_attachment:false/${result.public_id}.${extension}`;
-          }
-
+  
           resolve(result);
         },
       );
-
+  
       toStream(file.buffer).pipe(upload);
     });
   }
+  
 
   async deletePdf(
     publicId: string,
