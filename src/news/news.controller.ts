2fbdc9932c7b5a47ef1c962356e6211daba4c6f1
pavkas2024 <site.kasyanov@gmail.com>
2026-09-06 18:20:17 +0300

    update

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -122,7 +122,10 @@ export class NewsController {
           new ParseFilePipe({
               validators: [
                   new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 4 }),
-                  new FileTypeValidator({ fileType: /image\/(png|jpeg|jpg|webp)/ }),
+                  new FileTypeValidator({ 
+                    fileType: /image\/(png|jpeg|jpg|webp)/,
+                    skipMagicNumbersValidation: true, 
+                }),
               ],
               fileIsRequired: false,
           }),
