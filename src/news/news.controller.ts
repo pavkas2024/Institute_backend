75a063bc99546332cfa3969177d22d0240693420
pavkas2024 <site.kasyanov@gmail.com>
2026-09-06 18:16:11 +0300

    update bnews patch

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -122,7 +122,7 @@ export class NewsController {
           new ParseFilePipe({
               validators: [
                   new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 4 }),
-                  new FileTypeValidator({ fileType: '.(png|jpeg|jpg|webp)' }),
+                  new FileTypeValidator({ fileType: /image\/(png|jpeg|jpg|webp)/ }),
               ],
               fileIsRequired: false,
           }),
